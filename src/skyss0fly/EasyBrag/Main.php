366d67dab870ed26b2e4512515f63adfa032d257
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-07-15 12:59:19 +1000

    Update Main.php

--- src/skyss0fly/EasyBrag/Main.php
+++ src/skyss0fly/EasyBrag/Main.php
@@ -15,30 +15,30 @@ class Main extends PluginBase implements Listener {
 public function onLoad(): void {
 $this->saveDefaultConfig();
 }
-    public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool {
+
+public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
     switch ($command->getName()) {
-            case "brag":
-      $cooldown = $this->getConfig()->get("Cooldown");
-      $message =  $this->getConfig()->get("Message");
-      $player = $this->getServer()->getPlayerByPrefix($sender);
-      $item = $player->getInventory()->getItemInHand();
-  if (!$sender instanceof Player) {
-  $sender->sendMessage("Error, must be in game");
-    return false;
-  }
-      if( $cooldown < 0){
-        if($item !== null ){
-$bc = "$player . $message . $item";
-          $this->getServer()->broadcastMessage($bc);
-      return false;
-        }
-        return true;
-        }  
-      else{
-$sender->sendMessage("Error: still in timeout");
-        return false;
-      }
+        case "brag":
+            $cooldown = $this->getConfig()->get("Cooldown");
+            $message = $this->getConfig()->get("Message");
+            $player = $this->getServer()->getPlayerByPrefix($sender);
+            $item = $player->getInventory()->getItemInHand();
+
+            if (!$sender instanceof Player) {
+                $sender->sendMessage("Error, must be in game");
+                return false;
+            }
+
+            if ($cooldown < 0) {
+                if ($item !== null) {
+                    $bc = "$player . $message . $item";
+                    $this->getServer()->broadcastMessage($bc);
+                    return false;
+                }
+            } else {
+                $sender->sendMessage("Error: still in timeout");
+            }
     }
-    }
+}
 }
 
