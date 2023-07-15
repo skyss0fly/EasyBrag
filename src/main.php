9741f335c957106fc582da2b163af8984b86f3a9
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-07-15 11:37:33 +1000

    Create main.php

---
+++ src/main.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace skyss0fly\EasyBrag;
+
+use pocketmine\player\Player;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+use pocketmine\inventory\PlayerInventory;
+
+class Main extends PluginBase implements Listener {
+  /@var Config*/
+  private config;
+
+public function onLoad(): void {
+$this->saveDefaultConfig();
+  $config = $this->getConfig();
+  
+
+}
+public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool {
+  switch ($command->getName()) {
+            case "brag":
+    if(!$sender instanceof Player){
+  $sender->sendMessage("Error, must be in game");
+      
+    } 
+    $cooldown = $config->get("Cooldown");
+    $message = $config->get("Message");
+    $item = $sender->getItemInHand();
+    $this->getServer()->broadcastMessage($sender , $message , $item);
+    sleep($cooldown)
+      if (sleep($cooldown) < 0) {
+      $sender->sendMessage("Please wait until your cooldown ends!");
+      return true;
+}
+}
